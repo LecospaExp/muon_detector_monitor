1c2162344ad8b2cfa506cb09426baf8bc6facf7c
turingtw <kevin83113081620@gmail.com>
2017-11-10 17:41:59 +0800

    plot only one ch and set a stop threshold

--- distriMonitor.py
+++ distriMonitor.py
@@ -8,9 +8,13 @@ import numpy as np
 
 
 # parameter
-binRange = [0, 50]
-period = 5
-desc = 'th_0.3v'
+Vth 	= 10 	#mv
+ChNum 	= 1 	# channel number
+stopTh 	= 5 #hit
+period 	= 5 	#s
+
+# plot params
+binRange = [0, 30]
 
 def parseInt(x):
 	try:
@@ -19,73 +23,85 @@ def parseInt(x):
 		return None;
 def write2file(str):
 	print str
-	file = open("data.txt", "w");
+	file = open("data.txt", "a");
 	file.write(str+'\n');
 	file.close();
 
 SerCounter = serial.Serial('/dev/ttyUSB0',115200)
 SerCounter.flushInput()
-counterArr = [ [] for _ in range(9)]
-counter = [ 0 for _ in range(9)]
+counterArr = []
+counter = 0
 
 
-fig, axarr = plt.subplots(3, 3, figsize=(15,9))
-axarr = axarr.ravel()
+fig = plt.figure(figsize=(8,6))
 
 event = threading.Event()
-
+keep = True
 def countThread():
-	global counter, SerCounter
-	while True:
+	global counter, SerCounter, keep
+	while keep:
 		inputD = SerCounter.read()
 		newEvtCh = parseInt(inputD);
-		if newEvtCh is not None and newEvtCh-1 in range(9):
-			counter[newEvtCh-1]+=1
+		if newEvtCh is ChNum:
+			counter += 1
+			print newEvtCh
+	SerCounter.close()
 def countdownThread():
-	global event, period
-	while True:
-		time.sleep(period)
+	global event, period, keep
+	while keep:
+		time.sleep(period-0.005)
 		event.set()
 
 t1 = Timer(0, countdownThread)	
-t1.start()
 t = Timer(0, countThread)	
-t.start()
 
 strTime = time.time()
+totaltime = 0
+totalhit  = 0
+totalIter = 0
+totalMean = 0
+totalVar  = 0
+avgRate   = 0
+
+t.start()
+t1.start()
+counter = 0
+
 while True:
+	totalIter += 1
 	event.clear()
 	event.wait()
-	plt.suptitle('Total time:'+str(time.time()-strTime))
-	# print counter
+	fig.clf()
+
+		
+	totaltime = time.time()-strTime
+	counterArr+=[counter]
+	counter = 0
+	plt.suptitle('Total iter:'+str(totalIter)+' Total time:'+str(np.round(time.time()-strTime, 3))+ 's')
+	plt.title('CH_'+str(ChNum))
+	plt.xlabel("# of hit in "+str(period)+" sec")
+	plt.ylabel("Count")
+
+	entries, bin_edges, patches  = plt.hist(counterArr, range=binRange, bins=binRange[1], normed=True)
+
+	numHit = np.array(counterArr, dtype=np.float64)
+	totalhit, totalMean, totalVar = np.sum(numHit), np.round(np.mean(numHit),4), np.round(np.var(numHit),4)
+	avgRate = np.round(totalhit/totaltime,4)
+	text  = "Total "+str(totalhit)+"\n"
+	text += "Mean  "+str(totalMean)+"\n"
+	text += "Var   "+str(totalVar)+"\n"
+	text += "Rate  "+str(avgRate)+"\n"
+
+	plt.text(plt.gca().get_xlim()[1]-6, plt.gca().get_ylim()[1]/2, text,family="monospace",
+    bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+
 	
-	for idx, ax in enumerate(axarr):
-		counterArr[idx]+=[counter[idx]]
-
-
-		ax.clear()
-		ax.set_title('CH_'+str(idx+1))
-		if idx/3==2:
-			ax.set_xlabel("# of hit in "+str(period)+" sec")
-		if idx%3==0:
-			ax.set_ylabel("Count")
-		entries, bin_edges, patches  = ax.hist(counterArr[idx], range=binRange, bins=binRange[1], normed=True)
-
-		numHit = np.array(counterArr[idx], dtype=np.float64)
-		text  = "Total "+str(np.sum(numHit))+"\n"
-		text += "Mean  "+str(np.round(np.mean(numHit),4))+"\n"
-		text += "Var   "+str(np.round(np.var(numHit),4))+"\n"
-		text += "Rate  "+str(np.round(np.var(numHit)/5,4))+"\n"
-
-		ax.text(ax.get_xlim()[1]-20, ax.get_ylim()[1]/2, text,family="monospace",
-        bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
-
-	counter = [0 for _ in range(9)]
-
 
 
 	plt.draw()
-	plt.pause(0.01)
-	plt.savefig('noh_'+str(period)+'_s_'+desc+'.jpg')
-	
-SerCounter.close()
+	plt.pause(0.00000001)
+	plt.savefig('noh_'+str(period)+'_s_CH'+str(ChNum)+'_Vth-'+str(Vth)+'mv.jpg')
+	if(np.sum(numHit)>stopTh):
+		keep = False
+		break
+write2file(",".join(map(str, [Vth, avgRate, totalhit, totaltime, totalIter, totalMean, totalVar])))
